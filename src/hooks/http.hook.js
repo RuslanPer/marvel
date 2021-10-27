a78d683b0466a66c81b676bb72cfdfb0c0fcd26e
Ruslan <ruslan.pershin98@gmail.com>
2021-10-27 10:29:32 +0300

    optimization Helmet

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -4,7 +4,7 @@ export const useHttp = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const request = useCallback( async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
 
         setLoading(true);
 
@@ -19,16 +19,14 @@ export const useHttp = () => {
 
             setLoading(false);
             return data;
-
         } catch(e) {
             setLoading(false);
             setError(e.message);
             throw e;
         }
-
     }, []);
 
-    const clearError = useCallback( () => setError(null), []);
+    const clearError = useCallback(() => setError(null), []);
 
     return {loading, request, error, clearError}
 }
